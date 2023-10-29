d97e40de5dfa1cadde6c685be144c2cdc22370fe
GabrielTalledo <139196414+GabrielTalledo@users.noreply.github.com>
2023-10-29 06:27:18 -0500

    Update Prueba3.java
    Se añadio más atributos

--- Prueba3.java
+++ Prueba3.java
@@ -1,3 +1,9 @@
 public class Prueba3 {
     private String letra = "g";
+
+    private String nombre = "Floripundio";
+
+    private String apellido = "Campos";
+
+    private String codigo = "asd123";
 }

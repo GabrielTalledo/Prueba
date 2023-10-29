51f90daf1100a56d6e02a7ee45247228e8d74591
Gabriel <gabriel_alejandro.tm@hotmail.com>
2023-10-29 06:29:41 -0500

    Se implementó una letra "g"

--- Prueba3.java
+++ Prueba3.java
@@ -6,4 +6,16 @@ public class Prueba3 {
     private String apellido = "Campos";
 
     private String codigo = "asd123";
+
+    public void comer(String comida){
+
+        System.out.println("Estoy comiendo este rico plato:");
+        System.out.println(comida+" uwu");
+    }
+
+    public void dormir(){
+        System.out.println("Wa dormir");
+        System.out.println("zzzzzzzzz");
+
+    }
 }
